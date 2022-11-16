46e7f69d8e82a06072aa066737f8feb164679e42
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-16 01:09:06 +0000

    Fixed bug in binary tree insert left solution

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (!parent)
+		return (NULL);
 	node = binary_tree_node(parent, value);
 	if (!node)
 		return (node);
