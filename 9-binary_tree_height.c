ab208a7e96b10e035cc853d578849aa85ff091f5
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-16 09:22:38 +0000

    Added binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,14 @@
 
 size_t binary_tree_height_dfs(const binary_tree_t *tree)
 {
-	int left, right;
+	size_t left, right;
 
 	if (!tree)
 		return (0);
 
 	left = binary_tree_height_dfs(tree->left);
 	right = binary_tree_height_dfs(tree->right);
-	return (1 + ((left > right) ? left : right));
+	return ((left > right) ? left + 1 : right + 1);
 }
 
 /**
@@ -27,6 +27,8 @@ size_t binary_tree_height_dfs(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 	return (binary_tree_height_dfs(tree) - 1);
 }
 
