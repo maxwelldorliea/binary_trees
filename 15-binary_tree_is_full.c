b52b072e0fe03e9f5f51858d62d307451c23ce63
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-17 01:51:23 +0000

    Added binary tree is full solution

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: root of the tree
+ * Return: 1 or 0 on failure
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+	if ((tree->left) && (tree->right))
+		return (binary_tree_is_full(tree->left) &&
+	binary_tree_is_full(tree->right));
+
+	return (0);
+}
