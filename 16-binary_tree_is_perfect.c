26d1af1819cbc31fddfc47d0ee6dd53435cab82f
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-17 03:14:44 +0000

    Added binary tree is perfect solution

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+
+/**
+ * is_perfect - checks if a binary tree is perfect
+ * @tree: root of the tree
+ * @level: the level of the tree
+ * @depth: the depth
+ * Return: 1 or 0 on failure
+ */
+
+int is_perfect(const binary_tree_t *tree, int level, int depth)
+{
+	if (!tree)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (level + 1 == depth);
+	return (is_perfect(tree->left, level + 1, depth) &&
+			is_perfect(tree->right, level + 1, depth));
+}
+
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: root of the tree
+ * Return: 1 or 0 on failure
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int depth = 0, level = 0;
+	const binary_tree_t *saved_tree = tree;
+
+	if (!tree)
+		return (0);
+	while (tree)
+	{
+		depth++;
+		tree = tree->left;
+	}
+
+	tree = saved_tree;
+	return (is_perfect(tree, level, depth));
+}
