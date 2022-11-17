32d8b38ee35d198a9c51f52dc300518f7bd8c493
Maxwell D. Dorliea <deboy170@gmail.com>
2022-11-17 01:03:20 +0000

    Added binary tree balance solution

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_height_dfss - return the height of a tree
+ * @tree: root of the tree whose height is to be return
+ * Return: height of the tree
+ */
+
+size_t binary_tree_height_dfss(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+
+	left = binary_tree_height_dfss(tree->left);
+	right = binary_tree_height_dfss(tree->right);
+	return ((left > right) ? left + 1 : right + 1);
+}
 /**
  * binary_tree_balance - return the balance factor of a tree
  * @tree: root of the tree
@@ -13,8 +31,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	left = binary_tree_height_dfs(tree->left);
-	right = binary_tree_height_dfs(tree->right);
+	left = binary_tree_height_dfss(tree->left);
+	right = binary_tree_height_dfss(tree->right);
 
 	return (left - right);
 }
